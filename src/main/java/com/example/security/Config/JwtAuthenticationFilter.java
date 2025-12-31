088efc8f049db18ff01eb69d3d80bb05abc705dc
BARHAMA <suleimannmukhtar@gmail.com>
2025-12-31 14:07:32 +0100

    progress' git commit -m progress git commit -m progress git commit -m "progress"; git commit -m "progress" git push ; git commit -m "progress

--- src/main/java/com/example/security/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/Config/JwtAuthenticationFilter.java
@@ -9,12 +9,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain)
-                                    throws ServletException, IOException {
+            throws ServletException, IOException {
 
     }
 }
