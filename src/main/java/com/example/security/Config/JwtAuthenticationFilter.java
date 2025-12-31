9747e3f0615de8ce41d8ac0f6d7ef4c8c15e96f3
BARHAMA <suleimannmukhtar@gmail.com>
2025-12-31 15:27:26 +0100

    progress

--- src/main/java/com/example/security/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/Config/JwtAuthenticationFilter.java
@@ -16,12 +16,18 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
+    public final JwtService jwtService;
 
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
