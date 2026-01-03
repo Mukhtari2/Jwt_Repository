411841c5796c33d583d364f0f81c4b4214fa912d
BARHAMA <suleimannmukhtar@gmail.com>
2026-01-03 22:10:42 +0100

    progress

--- src/main/java/com/example/security/auth/AuthenticationController.java
+++ src/main/java/com/example/security/auth/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
     }
